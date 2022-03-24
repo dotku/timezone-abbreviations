ac2595835b413c66a9fecc8e60400bad276819c4
Weijing Lin <weijingjaylin@gmail.com>
2022-03-24 16:19:59 -0700

    should success publish

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,9 @@ import timezones from "./index";
 import timezoneData from "./data.json";
 
 test("check random item should match", () => {
-  const randomNumber = Math.floor(timezones.length * Math.random() * 10);
+  const randomNumber = Math.floor((timezones.length - 1) * Math.random() * 10);
   console.log("timezones", ...timezones.filter((item) => item.abbr === "AEST"));
-  expect(timezones[randomNumber].abbr).toBe(timezoneData[1].abbr);
+  expect(timezones[randomNumber].abbr).toBe(timezoneData[randomNumber].abbr);
   console.log("timezones type", typeof timezones[0]);
   console.log("timezones", ...timezones.filter((item) => item.abbr === "AEST"));
 });
