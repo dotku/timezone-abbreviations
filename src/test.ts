48247c7708305ed8b803026aa5f5a6b280a82e94
Weijing Lin <weijingjaylin@gmail.com>
2022-03-24 16:26:40 -0700

    use jest for test

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,11 @@ import timezones from "./index";
 import timezoneData from "./data.json";
 
 test("check random item should match", () => {
-  const randomNumber = Math.floor((timezones.length - 1) * Math.random() * 10);
-  console.log("timezones", ...timezones.filter((item) => item.abbr === "AEST"));
+  const randomNumber = Math.floor((timezones.length - 1) * Math.random());
+  console.log("length", timezones.length);
+  console.log("randomNumber", randomNumber);
   expect(timezones[randomNumber].abbr).toBe(timezoneData[randomNumber].abbr);
-  console.log("timezones type", typeof timezones[0]);
+
   console.log("timezones", ...timezones.filter((item) => item.abbr === "AEST"));
+  console.log("timezones type", typeof timezones[0]);
 });
