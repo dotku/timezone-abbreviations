32178df1bf9dfdb2caf46534e30b3821a663a309
Weijing Lin <weijingjaylin@gmail.com>
2022-03-24 11:52:42 -0700

    publish new build

--- dist/test.js
+++ dist/test.js
@@ -3,5 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const data_json_1 = __importDefault(require("./data.json"));
-exports.default = data_json_1.default;
+const index_1 = __importDefault(require("./index"));
+console.log("timezones", index_1.default);
+console.log("timezones", ...index_1.default.filter((item) => item.abbr === "AEST"));
